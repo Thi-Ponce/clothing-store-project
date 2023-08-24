4ed5a723475dcd21642eee93e60fdf1f93a0b475
Thi-Ponce <thi.pponce@gmail.com>
2023-08-24 17:13:12 -0300

    Install sass and start style main page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import './categories.style.scss';
+
 const App = () => {
 
   const categories = [
     {
       id: 1,
       title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Women',
+    },
+    {
+      id: 5,
+      title: 'Men',
     },
   ];
   
@@ -12,11 +30,11 @@ const App = () => {
     <div className="categories-container">
       {categories.map(({title}) => (
         <div className="category-container">
-        <h2>Category image</h2>
-        <div className="category-body-container">
-          <h3>{title}</h3>
-          <p>Category description</p>
-        </div>
+          <div className='background-image' />
+          <div className="category-body-container">
+            <h3>{title}</h3>
+            <p>Category description</p>
+          </div>
       </div>
       ))}
       
